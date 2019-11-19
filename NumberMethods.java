38e2f244779a44dbaadb59b160d16fb8c685ae9f
rmanushi <rmanushi@citycollge.sheffield.eu>
2019-11-19 09:17:55 +0200

    method signature in java

---
+++ NumberMethods.java
@@ -0,0 +1,39 @@
+public class NumberMethods
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	public static int minimumInt (int num1, int num2)
+	{
+		
+	}
+	public static int median (int num1, int num2, int num3)
+	{
+		
+	}
+	public static double average (int num1, int num2, int num3, int num4, int num5)
+	{
+
+	}
+	public static boolean isEven (int num)
+	{
+
+	}
+	public static int sumAllNumbersUpTo (int numberUpTo)
+	{
+
+	}
+	public static int sumEvenNumbersUpTo (int numberUpTo)
+	{
+
+	}
+	public static double percentage (int num, int numAsPercentage)
+	{
+
+	}
+	public static double solveEquation (int a, int b)
+	{
+		
+	}
+}
